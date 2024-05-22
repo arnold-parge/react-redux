06127ef3f944a21e03f7d15a445708ab57571e9a
Arnold Parge <arnoldparge@Arnolds-MacBook-Air.local>
2024-05-23 02:49:48 +0530

    Step 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './App.css';
 import { Counter } from './components/Counter';
-import { IncrementButton } from './components/Increment';
-import { DecrementButton } from './components/Decrement';
 import { CounterLoader } from './components/CounterLoader';
-import { UserForm } from './components/UserForm';
 
 function App() {
   return (
     <div className="App">
       <Counter />
-      <IncrementButton />
-      <DecrementButton />
       <CounterLoader />
     </div>
   );
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,18 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../data/store/AppStore";
+import { DecrementButton } from "./Decrement";
+import { IncrementButton } from "./Increment";
 
 export const Counter = () => {
     const count = useSelector((state: RootState) => state.counter.value);
     console.log('Counter rendered');
     return (
-        <div className="counter">
-            Count: {count}
-        </div>
+        <>
+            <div className="counter">
+                Count: {count}
+            </div>
+            <IncrementButton />
+            <DecrementButton />
+        </>
     );
 };
